314bdf48028b97efea11d0740ed453d49bc2969f
osinowo paul <osinowo62@gmail.com>
2021-01-24 23:51:08 +0100

    remove intel

--- config/settings.py
+++ config/settings.py
@@ -139,10 +139,3 @@ else:
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'osinowo62@gmail.com'
-# EMAIL_HOST_PASSWORD = 'photography999'
